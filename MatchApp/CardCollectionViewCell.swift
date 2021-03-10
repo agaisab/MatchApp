5102e406265b1b78860634d083b0559974b305a4
agaisab <agaisab@gmail.com>
2021-03-10 11:32:33 +0100

    Delegate/ Protocol Pattern implémentation in ViewCollection

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -9,4 +9,6 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var frontImageView: UIImageView!
+    @IBOutlet weak var backImageView: UIImageView!
 }
--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    
+    
+    @IBOutlet weak var collecionView: UICollectionView!
+    
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -17,8 +23,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.//
         
         cardsArray = model.getCards()
+        
+        // Set ViewController as the detasource and delegate of the collection view.
+        
+        collecionView.dataSource = self
+        collecionView.delegate = self
     }
 
-
+    
+    
+    // MARK: - Collecion View Methoths
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Return number of cards
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a cell
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Finish configue the cell
+        
+        // Return it
+        return cell
+    }
 }
 
