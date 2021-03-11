0dfdd87dd4f18ed3da450dc0186742d04550aed1
agaisab <agaisab@gmail.com>
2021-03-11 15:56:29 +0100

    Flipping logic

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -21,15 +21,33 @@ class CardCollectionViewCell: UICollectionViewCell {
         
         //Set the front image view to the image that represent the card
         frontImageView.image = UIImage(named: card.imageName)
+        
+        //reset the state of the cell, by checking flip status of the card and the showing the front or back.
+        if card.isFlipped == true {
+            //Show the front image view
+            flipUp(speed: 0)
+        
+        }
+        else{
+            flipDown(speed: 0)
+            
+        }
     }
     
-    func flipUp(){
+    func flipUp(speed:TimeInterval = 0.3){
         
-        //Flip up animation
-        UIView.transition(from: backImageView, to: frontImageView, duration: 0.4, options: [.showHideTransitionViews,.transitionFlipFromLeft], completion: nil)
+        // Flip up animation
+        UIView.transition(from: backImageView, to: frontImageView, duration: speed, options: [.showHideTransitionViews,.transitionFlipFromLeft], completion: nil)
+        
+        // set the status of the card
+        card?.isFlipped = true
     }
     
-    func flipDown() {
+    func flipDown(speed:TimeInterval = 0.3) {
         
+        UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews,.transitionFlipFromLeft], completion: nil)
+        
+        // Set the status of the card
+        card?.isFlipped = false
     }
 }
